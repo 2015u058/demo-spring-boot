edce291d9cb073e00c21bd197ec5a8c67bfe7975
2015u058 <50987189+2015u058@users.noreply.github.com>
2023-08-09 14:32:00 +0530

    Update CicdApplication.java

--- src/main/java/cicd/example/cicd/CicdApplication.java
+++ src/main/java/cicd/example/cicd/CicdApplication.java
@@ -2,12 +2,19 @@ package cicd.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 	}
 
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
 }
